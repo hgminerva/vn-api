6a8699560cfd3d373db5cdcd07f8f255a7104aa7
hgminerva <hgminerva@gmail.com>
2021-02-18 16:00:36 +0800

    soap

--- routes/web.php
+++ routes/web.php
@@ -40,44 +40,20 @@ Route::get('/soap/login/{employee_id}', function ($employee_id) {
 });
 
 Route::get('/soap/employee/{id}', function ($id) {
-
-/*     curl "https://www.mypinnaclecare.com:9443/VaxSvc.asmx/ReloadMembers" -X POST -
-    d "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n
-       <soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n  
-       <soap:Body>\n    
-       <ReloadMembers xmlns=\"http://tempuri.org/\" />\n  
-       </soap:Body>\n
-       </soap:Envelope>" */ 
-
-
-
-       $data='<?xml version="1.0" encoding="utf-8" ?>
-                <soap12:envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-                    <soap12:body>
-                        <MemberEditGet xmlns="http://tempuri.org/">
-                            <id>6</id>
-                        </MemberEditGet>
-                    </soap12:body>
-                </soap12:envelope>';
-
-
-
-
+    
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, 'https://www.mypinnaclecare.com:9443/VaxSvc.asmx/MemberEditGet');
+    curl_setopt($ch, CURLOPT_URL, 'https://www.mypinnaclecare.com:9443/VaxSvc.asmx/ReloadMembers');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     curl_setopt($ch, CURLOPT_POST, 1);
-    
+
     $headers = array();
-    $headers[] = 'Content-Type: text/xml; charset="utf-8"';
-    $headers[] = 'SOAPAction: "http://tempuri.org/MemberEditGet"';
+    $headers[] = 'Content-Type: text/xml; charset=utf-8';
     $headers[] = 'Content-Length: 309';
-    $headers[] = 'User-agent: advanced-rest-client';
+    $headers[] = 'User-Agent: advanced-rest-client';
     $headers[] = 'Accept: */*';
-    
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    
+
     $output = curl_exec($ch);
     curl_close ($ch);
 
