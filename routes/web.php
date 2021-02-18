cfb5c17d2cae81423a8cbba53a80134235fd3519
hgminerva <hgminerva@gmail.com>
2021-02-19 07:57:43 +0800

    script

--- routes/web.php
+++ routes/web.php
@@ -78,8 +78,8 @@ Route::get('/soap/employee/{id}', function ($id) {
 });
 
 Route::get('/script/scrapper', function () {
-    echo shell_exec('sudo /var/www/scraper/start-scraping.sh 2>&1');
-    echo exec("sudo /var/www/scraper/scrape.py 2>&1");
+    // echo shell_exec('sudo /var/www/scraper/start-scraping.sh 2>&1');
+    echo exec("sudo /usr/bin/python3 -u /var/www/scraper/scrape.py 2>&1");
 
 /*     $cmd = '/var/www/scraper/start_scraping.sh';
     while(@ ob_end_flush());
