0cf2a7ffb8e4c2bd7bba33f6acbcc09679a521c3
hgminerva <hgminerva@gmail.com>
2021-02-18 15:30:11 +0800

    soap

--- routes/web.php
+++ routes/web.php
@@ -57,7 +57,7 @@ Route::get('/soap/employee/{id}', function ($id) {
     curl_setopt($ch, CURLOPT_POST, 1);
     
     $headers = array();
-    $headers[] = 'Content-Type: application/x-www-form-urlencoded"';
+    $headers[] = 'Content-Type: application/soap+xml; charset=utf-8';
     $headers[] = 'Content-Length: 309';
     $headers[] = 'User-Agent: advanced-rest-client';
     $headers[] = 'Accept: */*';
