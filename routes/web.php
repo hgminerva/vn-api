d1c809a1e276c8891fb914286ef3ce0b0bb7f4b6
hgminerva <hgminerva@gmail.com>
2021-02-18 15:06:24 +0800

    soap

--- routes/web.php
+++ routes/web.php
@@ -35,32 +35,27 @@ Route::get('/soap/login/{employee_id}', function ($employee_id) {
     $output = curl_exec($ch);
     curl_close ($ch);
 
+    return response($output, 200)
+                  ->header('Content-Type', 'text/plain');
+});
 
-    // $data = json_encode(array(
-    //     "EmpID" => $employee_id
-    // ));
-    // $ch = curl_init();
-    // curl_setopt($ch, CURLOPT_URL, 'https://www.mypinnaclecare.com:9443/VaxSvc.asmx/Login?EmpID=' . $employee_id);
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    // curl_setopt($ch, CURLOPT_POST, true);
-    // curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    // $output = curl_exec($ch);
-    // curl_close($ch);
+Route::get('/soap/employee/{id}', function ($id) {
 
-    // $soapUrl = "https://www.mypinnaclecare.com:9443/VaxSvc.asmx/Login?EmpID=" . $employee_id; 
-    // $headers = array("Content-type: application/x-www-form-urlencoded",
-    //                  "Content-length: 13",
-    //                  "User-agent: advanced-rest-client",
-    //                  "Accept: */*"
-    //                 ); 
-
-    // $ch = curl_init();
-    // curl_setopt($ch, CURLOPT_URL, $soapUrl);
-    // curl_setopt($ch, CURLOPT_POST, true);
-    // curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-    // $response = curl_exec($ch); 
-    // curl_close($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://www.mypinnaclecare.com:9443/VaxSvc.asmx/MemberEditGet');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "ID=" . $id);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    
+    $headers = array();
+    $headers[] = 'Content-Type: text/xml; charset=utf-8';
+    $headers[] = 'Content-Length: 13';
+    $headers[] = 'User-Agent: advanced-rest-client';
+    $headers[] = 'Accept: */*';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    
+    $output = curl_exec($ch);
+    curl_close ($ch);
 
     return response($output, 200)
                   ->header('Content-Type', 'text/plain');
