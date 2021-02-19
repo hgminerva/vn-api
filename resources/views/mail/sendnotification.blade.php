baa8056dff9c89281ae33996bed35a3452891974
hgminerva <hgminerva@gmail.com>
2021-02-19 21:21:52 +0800

    email format

--- resources/views/mail/sendnotification.blade.php
+++ resources/views/mail/sendnotification.blade.php
@@ -4,7 +4,7 @@
 
 <div style="background-color: #efefef; padding: 45px 0px;">
 <div style="width: 600px; margin: 0px auto; background-color: #fff; border-radius: 10px; text-align: center; padding: 25px 25px 45px 25px;">
-<img style="margin-bottom: 35px;" src="https://vaccinetracker.pinnaclecare.com/assets/img/company_logo/pc2.png" />
+<img style="margin-bottom: 35px; height: 100px" src="https://vaccinetracker.pinnaclecare.com/assets/img/company_logo/pc2.png" />
 <div style="clear:both; height:15px;"></div>
 <div style="text-align:left">Good day! {{ $customer_user->name }}</div>
 <div style="clear:both; height:15px;"></div>
