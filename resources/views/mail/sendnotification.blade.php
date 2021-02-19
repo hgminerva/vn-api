2d72e28398b5945e70131b51895664150cee97af
hgminerva <hgminerva@gmail.com>
2021-02-19 21:19:18 +0800

    email formate

--- resources/views/mail/sendnotification.blade.php
+++ resources/views/mail/sendnotification.blade.php
@@ -1,9 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<h3>Good day! {{ $customer_user->name }}</h3>
 
-<div>
-    Vaccine Tracker:  You have the following match results:
+<div style="background-color: #efefef; padding: 45px 0px;">
+<div style="width: 600px; margin: 0px auto; background-color: #fff; border-radius: 10px; text-align: center; padding: 25px 25px 45px 25px;">
+<img style="margin-bottom: 35px;" src="https://vaccinetracker.pinnaclecare.com/assets/img/company_logo/pc2.png" />
+<div style="clear:both; height:15px;"></div>
+<div style="text-align:left">Good day! {{ $customer_user->name }}</div>
+<div style="clear:both; height:15px;"></div>
+<div style="text-align:left">Check your notifications below</div>
+<div style="clear:both; height:15px;"></div>
+<!-- Start -- LOOP THIS DIV -->
+<div class="loop-div-notif-entries" style="text-align:left; padding:20px; background:#eaeaea; border:1px solid #939494;">Notification Entry</div>
+<!-- End -- LOOP THIS DIV -->
+<div style="clear:both; height:15px;"></div>
 </div>
+</div>
+
 @endsection
