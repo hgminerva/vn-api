f03654a278a366807b403c4f58857d7b256e6663
hgminerva <hgminerva@gmail.com>
2021-02-21 19:12:55 +0800

    email logo

--- resources/views/mail/sendnotification.blade.php
+++ resources/views/mail/sendnotification.blade.php
@@ -6,7 +6,7 @@
 <div style="width: 600px; margin: 0px auto;text-align: center; ">
 <div style="background-image: url('https://vt.pinnaclecare.com/assets/img/email/banner-dark.png');background-position: center center; height: 175px;background-repeat: no-repeat;background-size: contain;">
 <div style="clear:both; height:100px; "></div>
-<img style="margin-left: 25px; margin-bottom: -10px; height: 40px; float: left;" src="https://vt.pinnaclecare.com/assets/img/email/logo.jpg" />
+<img style="margin-left: 25px; margin-bottom: -10px; height: 40px; float: left;" src="https://vt.pinnaclecare.com/assets/img/email/pc1.png" />
 <div style="clear:both; height:1px;"></div>
 <div style="text-align:left !important; padding-left: 120px !important; font-size:15px;font-family: calibri; color: #fffffe;">Vaccine Tracker Notification</div>
 </div>
