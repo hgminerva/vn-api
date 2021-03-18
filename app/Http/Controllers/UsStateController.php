04689c8b936383365a14ce4b9d32f1a531b19cf7
hgminerva <hgminerva@gmail.com>
2021-03-18 21:53:54 +0800

    u

--- app/Http/Controllers/UsStateController.php
+++ app/Http/Controllers/UsStateController.php
@@ -44,7 +44,7 @@ class UsStateController extends Controller
     {
 
         $us_states = UsState::join('vaccine_urls', 'us_states.id', '=', 'vaccine_urls.us_state_id')
-                            ->selectRaw('us_states.*, count(vaccine_urls.id) as total_urls')
+                            ->selectRaw('us_states.state_name, count(vaccine_urls.id) as total_urls')
                             ->groupBy('us_states.id')
                             ->get();
 
